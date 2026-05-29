c408185e13a77c3a56002dc8ba6a037317c9f6ee
lschiroli <l.schiroli@farm4trade.com>
2026-05-29 22:11:40 +0200

    test(extension): add attachShadow stub for jsdom

--- extension/tests/setup.js
+++ extension/tests/setup.js
@@ -3,3 +3,15 @@ if (!globalThis.CSS.escape) {
   globalThis.CSS.escape = (value) =>
     String(value).replace(/([!"#$%&'()*+,.\/:;<=>?@[\\\]^`{|}~])/g, '\\$1').replace(/^(\d)/, '\\3$1 ');
 }
+
+if (!HTMLElement.prototype.attachShadow) {
+  HTMLElement.prototype.attachShadow = function () {
+    const shadow = document.createElement('div');
+    shadow.__isShadowRoot = true;
+    this.appendChild(shadow);
+    Object.defineProperty(this, 'shadowRoot', { get: () => shadow, configurable: true });
+    // Forward getElementById/querySelector to shadow root
+    shadow.getElementById = (id) => shadow.querySelector(`#${id}`);
+    return shadow;
+  };
+}
