a1dc789016de64f262582295ce31d14cabed8683
lschiroli <l.schiroli@farm4trade.com>
2026-05-30 10:01:20 +0200

    chore(extension): remove Shadow DOM content script, clean up setup.js

--- extension/tests/setup.js
+++ extension/tests/setup.js
@@ -1,5 +1,4 @@
 // Expose jest as a globalThis property for ESM test files
-// (--experimental-vm-modules does not inject jest into module scope automatically)
 import { jest as _jest } from '@jest/globals';
 globalThis.jest = _jest;
 
@@ -7,16 +6,4 @@ if (!globalThis.CSS) globalThis.CSS = {};
 if (!globalThis.CSS.escape) {
   globalThis.CSS.escape = (value) =>
     String(value).replace(/([!"#$%&'()*+,.\/:;<=>?@[\\\]^`{|}~])/g, '\\$1').replace(/^(\d)/, '\\3$1 ');
-}
-
-if (!HTMLElement.prototype.attachShadow) {
-  HTMLElement.prototype.attachShadow = function () {
-    const shadow = document.createElement('div');
-    shadow.__isShadowRoot = true;
-    this.appendChild(shadow);
-    Object.defineProperty(this, 'shadowRoot', { get: () => shadow, configurable: true });
-    // Forward getElementById/querySelector to shadow root
-    shadow.getElementById = (id) => shadow.querySelector(`#${id}`);
-    return shadow;
-  };
 }
