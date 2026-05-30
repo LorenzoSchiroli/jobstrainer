a4f4d7a4471ed7076b0674b316bfea7c4c151e5e
lschiroli <l.schiroli@farm4trade.com>
2026-05-30 09:09:03 +0200

    fix(extension): declare side_panel.css as web_accessible_resource, remove unused webNavigation permission

--- extension/background/service_worker.js
+++ extension/background/service_worker.js
@@ -53,7 +53,6 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
         target: { tabId },
         files: ['content/dom_inspector.js', 'content/form_filler.js', 'content/side_panel.js'],
       });
-      await chrome.scripting.insertCSS({ target: { tabId }, files: ['content/side_panel.css'] });
       injectedTabs.add(tabId);
     } catch (_) {
       return; // Tab not injectable (e.g., chrome:// URL, PDF)
